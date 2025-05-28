19e471bc20b87d0ea95816248526770ab6ebdaa3
Shivanisingh2209 <143741203+Shivanisingh2209@users.noreply.github.com>
2025-05-28 10:02:51 +0530

    updated

--- lib/screen/tabs.dart
+++ lib/screen/tabs.dart
@@ -5,7 +5,6 @@ import 'package:taste_track/screen/filters.dart';
 import 'package:taste_track/screen/meals.dart';
 import 'package:taste_track/widgets/main_drawer.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:taste_track/providers/meals_provider.dart';
 import 'package:taste_track/providers/filters_provider.dart';
 
 const kInitialFilters = {
@@ -46,15 +45,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) return false;
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) return false;
-      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) return false;
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) return false;
-      return true;
-    }).toList();
+    final availableMeals = ref.watch(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
